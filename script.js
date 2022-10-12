c5e2655b62c1ecff9c7f466a603ba969d15c59a6
zettacamp-putra-rohadi <putra.bagaspati@zettacamp.pro>
2022-10-12 17:19:09 +0700

    task JS_day3

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-function book(bookName, discount, tax, bookstock, bookpurchase) {
+function book(bookName, discount, tax, bookstock, bookpurchase, terms) {
   let price = 1000;
   const totalDiscount = (price * discount) / 100;
   const priceAfterDiscount = price - totalDiscount;
   const totalTax = (priceAfterDiscount * tax) / 100;
   price = priceAfterDiscount + totalTax;
   let totalPrice = 0;
-
   for (let i = 0; i < bookpurchase; i++) {
     totalPrice = totalPrice + price;
     if (bookstock === 0) {
       return `You cannot buy more than in stock. 
       \nStock = ${bookstock}
-      \nYou Bought = ${i} books
-      \nTotal Price = ${totalPrice}`;
+      \nYou Bought = ${i} books, ${bookpurchase-i} books left.`;
+      // \nTotal Price = ${totalPrice}`;
     } else {
       totalPrice += price;
       bookstock--;
     }
   }
-  return `Total Price = ${totalPrice}
-      \nLeft in stock = ${bookstock}`;
+  const convTotalPrice = [totalPrice];
+  const totalPrice2 = convTotalPrice.map(x => x / terms)
+  // const term = (totalPrice) => totalPrice / terms;
+  termEachMonth = totalPrice2[0];
+  let creditPay = 0;
+  let credit = [];
+  for (let i = 0; i < terms; i++) {
+    credit[i] = 
+      {
+      Month : i+1,
+      'Credit To Be Paid' : termEachMonth,
+      'Credit Already Paid' : creditPay =+ termEachMonth,
+      Remaining : totalPrice -= termEachMonth,
+      };
+    }
+  console.log(credit);
+  const {Month} = credit[1];     //destructuring
+  console.log("Bulan ke-",Month);
 };
 
-const detail = book("And",10,5,2,3);
-console.log(detail);
+const bookInformation = ["The Legend",10,5,4,3,5]       //spread
+const detail = book(...bookInformation);
+
